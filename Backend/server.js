0ac1db6c752c0ebdf0364bbdf9de470eacd9f588
Irshad <may.irshad@gmail.com>
2023-11-26 19:51:18 -0800

    Completed the set up

--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,11 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-
+const jwt = require('jsonwebtoken')
 const User = require('./models/userModel')
 
 
+const SECRET_KEY = 'super-secret-key'
 
 // connect to express app
 const app = express()
@@ -32,21 +33,24 @@ app.use(bodyParser.json())
 app.use(cors())
 
 
-//////// Routes
+
+
+//Routes
 
 // REGISTER
-//POST REGISTER
+// POST REGISTER
 app.post('/register', async (req, res) => {
     try {
-        const { email, username, password } = req.body
-        const hashedPassword = await bcrypt.hash(password, 10)
-        const newUser = new User({ email, username, password: hashedPassword })
-        await newUser.save()
-        res.status(201).json({ message: 'User created successfully' })
+        const { email, username, password } = req.body;
+        // Storing the password without hashing
+        const newUser = new User({ email, username, password });
+        await newUser.save();
+        res.status(201).json({ message: 'User created successfully' });
     } catch (error) {
-        res.status(500).json({ error: 'Error signing up' })
+        res.status(500).json({ error: 'Error signing up' });
     }
-})
+});
+
 
 //GET Registered Users
 app.get('/register', async (req, res) => {
@@ -59,21 +63,22 @@ app.get('/register', async (req, res) => {
     }
 })
 
-    //LOGIN
+
+// LOGIN
 app.post('/login', async (req, res) => {
     try {
-        const { username, password } = req.body
-        const user = await User.findOne({ username })
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
         if (!user) {
-            return res.status(401).json({ error: 'Invalid credentials'})
+            return res.status(401).json({ error: 'Invalid credentials' });
         }
-        const isPasswordValid = await bcrypt.compare(password, user.password)
-        if(!isPasswordValid) {
-            return res.status(401).json({ error: 'Invalid credentials' })
+        // Directly comparing the plaintext passwords
+        if (password !== user.password) {
+            return res.status(401).json({ error: 'Invalid credentials' });
         }
-        const token = jwt.sign({ userId: user._id }, SECRET_KEY, { expiresIn: '1hr' })
-        res.json({ message: 'Login successful' })
+        const token = jwt.sign({ userId: user._id }, SECRET_KEY, { expiresIn: '1hr' });
+        res.json({ message: 'Login successful' });
     } catch (error) {
-        res.status(500).json({ error: 'Error logging in' })
+        res.status(500).json({ error: 'Error logging in' });
     }
-})
+});
