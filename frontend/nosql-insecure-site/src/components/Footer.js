56f0ca0f073e208c7bd22907004b5c9642bd48b6
Irshad <may.irshad@gmail.com>
2023-11-26 22:45:49 -0800

    fixed footer text

--- frontend/nosql-insecure-site/src/components/Footer.js
+++ frontend/nosql-insecure-site/src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
         <footer className='w-full bg-gradient-to-r from-blue-800 to-teal-700 text-white py-4 px-6 border-t border-blue-900 shadow-md'>
             <div className='container mx-auto text-center'>
                 <h3 className='text-xl font-bold'>NoSQL-DB Exploit</h3>
-                <p className='text-md mt-1'>© {currentYear} Ahmad Irshad Mayaar. All Rights Reserved.</p>
+                <p className='text-md mt-1'>All Rights Reserved © {currentYear}. Ahmad Irshad Mayaar.</p>
             </div>
         </footer>
     );
